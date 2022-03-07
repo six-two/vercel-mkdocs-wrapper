8765f299de510a4a212c254154c44466564bc205
six-two <git@six-two.dev>
2022-03-07 20:41:36 +0100

    Added tags, better default pages and more debugging outputs

--- build.sh
+++ build.sh
@@ -24,9 +24,6 @@ sed "s/<NAME>/$NAME/" mkdocs-template.yml > mkdocs.yml
 REPO=${DOCS_REPO:-https://github.com/six-two/notes-demo-page}
 REPO=${1:-$REPO}
 
-# Install the static site generator and the theme
-pip install mkdocs mkdocs-material mkdocs-minify-plugin mkdocs-autolinks-plugin
-
 if [[ -d "${REPO}" ]]; then
   # allows using a local folder for more flexible use (and for easy local testing)
   echo "[build.sh] Using docs folder: ${REPO}"
@@ -38,13 +35,42 @@ else
   git clone "${REPO}" docs
 fi
 
+echo "[build.sh] Installing dependencies"
+# Install the static site generator and the theme
+pip install mkdocs mkdocs-material mkdocs-minify-plugin mkdocs-autolinks-plugin
+
+
 # Delete the README.md file, since it often contains unnecessary stuff, such as installation instructions.
 # If you want an index page, create a file called 'index.md' instead.
 rm docs/README.md
 # if no index.md file exists, create an empty page
 if [ ! -f docs/index.md ]; then
-  echo "# $NAME" > docs/index.md
+  cat << EOF > docs/index.md
+# ${NAME}
+
+## Navigating
+On the left side, you can see a list of all pages.
+On the right side, you can see the different sections of the current page.
+Click on any of the sections to jump there.
+
+## Searching
+You can also search this page.
+Just type \`/\` or click on the search bar at the top right corner te start searching.
+EOF
 fi
 
+# if no tags.md file exists, create an standard one
+if [ ! -f docs/tags.md ]; then
+  cat << EOF > docs/tags.md
+# Find a page by tag
+The following tags are defined. Use the table of contents on the right side to jump
+to the tag you are interested in:
+[TAGS]
+EOF
+fi
+
+echo "[build.sh] Building site"
 # Build the site. The result will be written in the ./public directory
 mkdocs build -d public
+
+echo "[build.sh] Done"
